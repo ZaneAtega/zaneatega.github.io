d82380aef108a95baebb58d37ae701263dbe96a7
zaneatega <zfaoatega@gmail.com>
2024-07-16 15:57:28 +0800

    sitemaps

--- sitemaps/generate_sitemaps.py
+++ sitemaps/generate_sitemaps.py
@@ -1,7 +1,7 @@
-with open('github.io.xml', 'r', encoding='utf-8') as file: content = file.read()
+with open('github.io.xml', 'r') as file: content = file.read()
 
 replacements = ['pages.dev','vercel.app','onrender.com','netlify.app']
 
 for replacement in replacements:
-    with open(replacement + '.xml', 'w', encoding='utf-8') as file:
+    with open(replacement + '.xml', 'w') as file:
         file.write(content.replace('github.io',replacement))
