d53106e29580abb950f707d7bcfc70c57dcb0f39
zaneatega <zfaoatega@gmail.com>
2026-06-16 03:24:59 +0800

    Your site shouldn’t look like everyone else’s.

--- sitemaps/generate_sitemaps.py
+++
@@ -1,7 +0,0 @@
-with open('github.io.xml', 'r') as file: content = file.read()
-
-replacements = ['pages.dev','vercel.app','onrender.com','netlify.app']
-
-for replacement in replacements:
-    with open(replacement + '.xml', 'w') as file:
-        file.write(content.replace('github.io',replacement))
